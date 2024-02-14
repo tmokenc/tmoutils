4e5bed21b2d2c59f7b45b27a6b569bc1ba0f34a5
tmokenc <tmokenc@protonmail.com>
2024-02-14 09:00:59 +0100

    Add renamer

---
+++ renamer/src/lib.rs
@@ -0,0 +1,105 @@
+mod walkdir;
+
+use clap::*;
+use std::cmp::Reverse;
+use std::fs;
+use std::io;
+use walkdir::WalkDir;
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+pub enum FileType {
+    Folder,
+    File,
+    #[default]
+    All,
+}
+
+#[derive(Debug, Parser)]
+/// Put all files in the `location` into folder with its name, multiple files that has the same
+/// name (but different extension) will go into the same folder
+pub struct Args {
+    /// Regex pattern to match
+    pattern: String,
+
+    #[arg(long, short, default_value_t = String::new())]
+    /// replace the matches pattern by
+    replace: String,
+
+    #[arg(long, short, value_enum, default_value_t = FileType::File)]
+    r#type: FileType,
+    /// The base location to start with, default to the current one
+    #[arg(long, short, default_value = ".")]
+    location: String,
+
+    #[arg(long, short)]
+    depth: Option<usize>,
+
+    #[arg(long, default_value_t = false)]
+    /// Follow symlink or not
+    follow: bool,
+}
+
+impl Args {
+    pub fn exec(&self) -> anyhow::Result<()> {
+        let regex = regex::Regex::new(&self.pattern)?;
+        let mut todolist = Vec::new();
+
+        for item in WalkDir::new(&self.location, self.depth)? {
+            let Ok(metadata) = item.metadata() else {
+                continue;
+            };
+
+            if !self.follow && metadata.is_symlink() {
+                continue;
+            }
+
+            if matches!(
+                (self.r#type, metadata.is_dir()),
+                (FileType::File, true) | (FileType::Folder, false)
+            ) {
+                continue;
+            }
+
+            if matches!(
+                (self.r#type, metadata.is_file()),
+                (FileType::File, false) | (FileType::Folder, true)
+            ) {
+                continue;
+            }
+
+            let path = item.path();
+            let Some(filename) = path.file_name().and_then(|v| v.to_str()) else {
+                continue;
+            };
+
+            if !regex.is_match(filename) {
+                continue;
+            }
+
+            let new_name = regex.replace_all(filename, &self.replace);
+            let new_path = path.parent().unwrap().join(&*new_name);
+
+            todolist.push((path, new_path));
+        }
+
+        todolist.sort_by_key(|(current, _new)| Reverse(current.components().count()));
+
+        for (i, (current, new)) in todolist.iter().enumerate() {
+            log::info!("{i}: {:#?}\n=> {:#?}", &current, &new);
+        }
+
+        print!("Process? (Y/else): ");
+        let mut stdin = io::stdin().lines();
+        if let Some(Ok(line)) = stdin.next() {
+            if matches!(line.trim(), "y" | "yes") {
+                for (current, new) in todolist {
+                    if let Err(why) = fs::rename(current, new) {
+                        log::error!("Error why renaming file\n{:#?}", why);
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
---
+++ renamer/src/walkdir.rs
@@ -0,0 +1,83 @@
+use anyhow::Result;
+use std::fs;
+use std::path::{Path, PathBuf};
+
+struct NextDir {
+    path: PathBuf,
+    depth: usize,
+}
+
+pub struct WalkDir {
+    iter: fs::ReadDir,
+    folders: Vec<NextDir>,
+    depth: usize,
+    max_depth: Option<usize>,
+}
+
+impl WalkDir {
+    pub fn new(path: impl AsRef<Path>, max_depth: impl Into<Option<usize>>) -> Result<Self> {
+        Ok(Self {
+            iter: fs::read_dir(path)?,
+            folders: Vec::new(),
+            depth: 0,
+            max_depth: max_depth.into(),
+        })
+    }
+
+    fn next_in_queue(&mut self) -> Option<()> {
+        if self.folders.is_empty() {
+            return None;
+        }
+
+        while let Some(next) = self.folders.pop() {
+            match fs::read_dir(next.path) {
+                Ok(iter) => {
+                    self.iter = iter;
+                    self.depth = next.depth;
+                    return Some(());
+                }
+
+                Err(why) => {
+                    log::error!("Cannot read a directory entry\n{:?}", why);
+                }
+            }
+        }
+
+        None
+    }
+}
+
+impl Iterator for WalkDir {
+    type Item = fs::DirEntry;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let Some(next) = self.iter.next() else {
+                self.next_in_queue()?;
+                continue;
+            };
+
+            match next {
+                Ok(item) => {
+                    let Ok(metadata) = item.metadata() else {
+                        continue;
+                    };
+
+                    if metadata.is_dir() {
+                        if matches!(self.max_depth, Some(max) if max < self.depth) {
+                            self.folders.push(NextDir {
+                                path: item.path(),
+                                depth: self.depth + 1,
+                            });
+                        }
+                    }
+
+                    return Some(item);
+                }
+                Err(why) => {
+                    log::error!("Cannot read a directory entry\n{:?}", why);
+                }
+            }
+        }
+    }
+}
