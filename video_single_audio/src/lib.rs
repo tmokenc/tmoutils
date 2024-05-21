a915054294b816e29f5c1af33f6b6679a3b2fced
tmokenc <tmokenc@protonmail.com>
2024-05-21 09:03:57 +0200

    feat: add logging

--- video_single_audio/src/lib.rs
+++ video_single_audio/src/lib.rs
@@ -1,5 +1,7 @@
 use clap::*;
+use human_bytes::human_bytes;
 use serde::*;
+use std::fs;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 use walkdir::WalkDir;
@@ -53,7 +55,15 @@ pub struct Args {
     /// Where to store the result
     output_dir: String,
 
-    #[arg(long, short, default_value_t = String::from("ja"))]
+    #[arg(long, short, default_value_t = false)]
+    /// Replace the original files
+    replace: bool,
+
+    #[arg(long, short, default_value_t = 1)]
+    /// Recursive done
+    depth: usize,
+
+    #[arg(long, short, default_value_t = String::from("jpn"))]
     audio_name: String,
 }
 
@@ -61,9 +71,12 @@ impl Args {
     pub fn exec(&self) -> anyhow::Result<()> {
         let iter = WalkDir::new(&self.location)
             .sort_by_file_name()
+            .max_depth(self.depth)
             .into_iter()
             .filter_map(Result::ok)
             .filter(|v| v.path().extension().filter(|x| x == &"mkv").is_some());
+
+        let mut saved = 0;
 
         for entry in iter {
             let mut info = MkvInfo::from_path(entry.path())?;
@@ -85,16 +98,26 @@ impl Args {
             if let Some(track_id) = maybe_track_id {
                 log::info!("Processing {:#?}", entry.path());
 
-                if let Err(why) = self.retain_audio(entry.path(), track_id) {
-                    log::error!("Failed to process a video\n{:#?}", why);
+                match self.retain_audio(entry.path(), track_id) {
+                    Ok((original, retained)) => {
+                        log::info!(
+                            "{} => {}\n",
+                            human_bytes(original as f64),
+                            human_bytes(retained as f64)
+                        );
+                        saved += original - retained;
+                    }
+                    Err(why) => log::error!("Failed to process a video\n{:#?}", why),
                 }
             }
         }
 
+        log::info!("Saved total {}", human_bytes(saved as f64));
+
         Ok(())
     }
 
-    fn retain_audio(&self, path: impl AsRef<Path>, track_id: u64) -> anyhow::Result<()> {
+    fn retain_audio(&self, path: impl AsRef<Path>, track_id: u64) -> anyhow::Result<(u64, u64)> {
         let path = path.as_ref();
         let filename = path.file_name().unwrap();
 
@@ -103,12 +126,21 @@ impl Args {
 
         Command::new("mkvmerge")
             .arg("-o")
-            .arg(output_path)
+            .arg(&output_path)
             .arg("-a")
             .arg(track_id.to_string())
             .arg(path)
             .output()?;
 
-        Ok(())
+        let original = path.metadata()?.len();
+        let retained = output_path.metadata()?.len();
+
+        if self.replace {
+            log::info!("Replacing");
+            fs::remove_file(path)?;
+            fs::rename(output_path, path)?;
+        }
+
+        Ok((original, retained))
     }
 }
